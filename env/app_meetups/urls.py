106708b2e70b81029601bedfc31f3a7809ee3a75
DESKTOP-JQAIFQV\User <carlospereira.ti@gmail.com>
2022-03-05 18:23:26 -0300

    Dynamic URLs in Templates - urls tag and interpolation - done working

--- env/app_meetups/urls.py
+++ env/app_meetups/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('meetups/', views.index), # domain.com/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_details), # domain.com/meetups/<dynamicsegment>
+    path('meetups/', views.index, name='all-meetups'), # domain.com/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'), # domain.com/meetups/<dynamicsegment>
 ]
