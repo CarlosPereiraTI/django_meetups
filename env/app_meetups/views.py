fe1b9421248612ed281136a80fc4e1c11ccb5b1f
DESKTOP-JQAIFQV\User <carlospereira.ti@gmail.com>
2022-03-17 14:37:43 -0300

    Querying Data 02:07:39

--- env/app_meetups/views.py
+++ env/app_meetups/views.py
@@ -1,36 +1,27 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Meetup
 
 # Create your views here.
 
 
 def index(request):
-    # dictionary containing meetups information
-    meetups = [
-        { 
-            'title': '1st meetup',
-            'location': 'New York',
-            'slug': 'a-fst-meetup' 
-        },
-        { 
-            'title': '2nd meetup',
-            'location': 'Paris',
-            'slug': 'a-snd-meetup' 
-        }
-    ]
-
+    meetups = Meetup.objects.all()
     return render(request, "meetups/index.html", {
-        'show_meetups': True,
+        # 'show_meetups': True,
         'meetups': meetups
     })
 
 
 def meetup_details(request, meetup_slug):
-    selected_meetup = {
-        'title': 'First meetup',
-        'description': 'This is the first meetup'
-        }
-    return render(request, 'meetups/meetup-details.html', {
-        'meetup_title': selected_meetup['title'],
-        'meetup_description': selected_meetup['description'],
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': True,
+            'meetup_title': selected_meetup.title,
+            'meetup_description': selected_meetup.description
+        })
+    except Exception as exc:
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup_found': False,
+        })
